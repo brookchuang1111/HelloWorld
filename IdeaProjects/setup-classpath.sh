9f50aabe345dc618873ccaed803f309d2a278bf3
Sriram Rao <sriramrao@fb.com>
2018-07-08 10:54:25 -0700

    Print out what needs to be done to setup CLASSPATH.

--- IdeaProjects/setup-classpath.sh
+++ IdeaProjects/setup-classpath.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 export CLASSPATH=`pwd`/libs/stdlib.jar:`pwd`/code/out/production/code
+classpath_stmt="$(echo export CLASSPATH=$CLASSPATH)"
+echo "Cut and paste the following line:"
+echo $classpath_stmt
