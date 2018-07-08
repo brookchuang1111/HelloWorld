6d31411da758f3d4094655fb1e31281de092d875
Sriram Rao <sriramrao@fb.com>
2018-07-08 11:04:32 -0700

    Update classpath to include new modules that are created.

--- IdeaProjects/setup-classpath.sh
+++ IdeaProjects/setup-classpath.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export CLASSPATH=`pwd`/libs/stdlib.jar:`pwd`/code/out/production/code
+CLASSPATH=`pwd`/libs/stdlib.jar
+for i in `pwd`/code/out/production/* 
+do 
+  CLASSPATH=$CLASSPATH:$i
+done
 classpath_stmt="$(echo export CLASSPATH=$CLASSPATH)"
 echo "Cut and paste the following line:"
 echo $classpath_stmt
