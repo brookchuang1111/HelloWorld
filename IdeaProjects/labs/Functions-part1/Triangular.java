fb5708131898b57628ff4989c78dd504359e443d
Sriram Rao <sriramrao@fb.com>
2018-07-14 15:33:18 -0700

    Added Triangular.java

---
+++ IdeaProjects/labs/Functions-part1/Triangular.java
@@ -0,0 +1,24 @@
+/*
+ * Name:
+ *
+ * Description:
+ *
+ */
+
+public class Triangular {
+
+    // TODO: Write a method areTriangular() that takes 3 doubles as arguments
+    // and returns true if they could be sides of a triangle.
+    // HINT: Use triangular inequality.
+
+    public static void main(String args[]) {
+        // Take 3 doubles as command line arguments.
+        // Inputs to test your program:
+        //     3 4 5   -> true
+        //     1 2 3   -> false
+
+        // Call the areTriangular() method and print the result
+
+
+    }
+}
