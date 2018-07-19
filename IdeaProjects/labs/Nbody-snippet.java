f8785430e081bd6faab1f3fa68a1c8eab2eb56e5
Sriram Rao <sriramrao@fb.com>
2018-07-19 07:13:24 -0700

    Fix the force computation so that the direction is properly included.

--- IdeaProjects/labs/Nbody-snippet.java
+++ IdeaProjects/labs/Nbody-snippet.java
@@ -23,20 +23,18 @@ The following is a hint at what you will need to compute forces.
                   if (i == j)
                     continue;
                   r[i][j] = Math.sqrt((px[i] - px[j]) * (px[i] - px[j]) + (py[i] - py[j]) * (py[i] - py[j]));
-                  // Compute the force between i and j
+                  // Compute the magnitude of force between i and j
                   pairwiseForces[i][j] = (G * mass[i] * mass[j]) / (r[i][j] * r[i][j]) ;
-		  // Note that the force between i and j is in the opposite direction of
-		  // of the force between j and i.
-		  if (j > i)
-		  	// flip the sign
-		  	pairwiseForces[i][j] *= -1.0;
             }
 
        	  for (int i = 0; i < N; i++) {
             fx[i] = 0.0;
             for (int j = 0; j < N; j++) {
                 // Compute F cos theta.
-                fx[i] += pairwiseForces[i][j] * ((px[i] - px[j]) / r[i][j];
+		// NOTE: to get the direction right, we compute px[j] - px[i]
+		// This computes the force exerted by object J on object I.
+		// Similarly for y direction.
+                fx[i] += pairwiseForces[i][j] * ((px[j] - px[i]) / r[i][j];
             }
           }
 	}
